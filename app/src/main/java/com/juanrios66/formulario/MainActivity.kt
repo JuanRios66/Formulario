4adfa18d71e5cd6a38d124963df88147957f58b9
JDRIOS <judari_66@hotmail.com>
2021-03-11 11:44:53 -0500

    second

--- app/src/main/java/com/juanrios66/formulario/MainActivity.kt
+++ app/src/main/java/com/juanrios66/formulario/MainActivity.kt
@@ -1,25 +1,34 @@
 package com.juanrios66.formulario
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.juanrios66.formulario.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        private lateinit var mainBinding = ActivityMainBinding
-
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        mainBinding.save_Button.setOnClickListener{it:View
-            var email = mainBinding.email_Text.text.toString()
-            var password = mainBinding.pass_text.text.toString()
-            var repeat_pass = mainBinding.repeat_pass_text.text.toString()
+        mainBinding.saveButton.setOnClickListener {
+            var email = mainBinding.emailText.text.toString()
+            var password = mainBinding.passText.text.toString()
+            var repeatpass = mainBinding.repeatPassText.text.toString()
 
-
+            if (email.isNotEmpty() and password.isNotEmpty()) {
+                mainBinding.repeatPass.error = null
+                if (password == repeatpass || repeatpass.isNotEmpty()) {
+                    mainBinding.textView2.text = email
+                } else {
+                    mainBinding.repeatPass.error = getString(R.string.error)
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.empty), Toast.LENGTH_LONG).show()
+            }
         }
 
 
